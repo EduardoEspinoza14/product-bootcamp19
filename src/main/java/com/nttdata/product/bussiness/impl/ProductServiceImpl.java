e5f9af15df407465a14e4ba0f13ff80b3087a4d0
EduardoEspinoza14 <eduardoantonio.espinozallontop@everis.nttdata.com>
2022-06-07 14:58:57 -0500

    Commit

--- src/main/java/com/nttdata/product/bussiness/impl/ProductServiceImpl.java
+++ src/main/java/com/nttdata/product/bussiness/impl/ProductServiceImpl.java
@@ -3,13 +3,12 @@ package com.nttdata.product.bussiness.impl;
 import com.nttdata.product.bussiness.ProductService;
 import com.nttdata.product.model.mongo.ProductMongo;
 import com.nttdata.product.repository.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Service
-public class ProductServiceImpl implements ProductService {
+public abstract class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
@@ -19,10 +18,34 @@ public class ProductServiceImpl implements ProductService {
         return productRepository.findAll();
     }
 
+    public Flux<ProductMongo> getProductsByType(String type) {
+        return productRepository.findByType(type);
+    }
+
     @Override
-    public Mono<ProductMongo> insertProduct(Mono<ProductMongo> product) {
-        return product.flatMap(productRepository::insert);
-        //return productRepository.insert(product);
+    public Mono<ProductMongo> getProduct(String id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Mono<ProductMongo> insertProduct(ProductMongo product) {
+        return productRepository.insert(product);
+    }
+
+    @Override
+    public Mono<ProductMongo> updateProduct(ProductMongo product, String id) {
+        return productRepository.findById(id)
+                .map(p_db -> {
+                    BeanUtils.copyProperties(product, p_db, "id", "type");
+                    return p_db;
+                })
+                .flatMap(productRepository::save);
+    }
+
+    @Override
+    public Mono<Void> deleteProduct(String id) {
+        return productRepository.findById(id)
+                .flatMap(p->productRepository.deleteById(p.getId()));
     }
 
 }
